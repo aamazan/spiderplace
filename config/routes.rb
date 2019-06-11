dab074d20f3f9b3ed7c6e26c184a1b6eb58d040d
Armaan Amazan <armaan.amazan@gmail.com>
2019-06-11 12:56:14 -0700

    generated review model, switching to branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/contact'
-  get 'pages/about'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'reviews,     to: 'reviews#index'
+  post 'reviews',    to: 'reviews#create'
+  get 'reviews/:id', to: 'reviews#show', as: :review
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
